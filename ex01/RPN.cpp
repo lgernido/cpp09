15f1ea7449554129a7bbc066ce7b09a72b97fb47
Lucie Gernidos <lgernido@made-f0Cr7s1.clusters.42paris.fr>
2024-06-25 09:19:40 +0200

    ex01 finished

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,8 +6,102 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 15:44:16 by lgernido          #+#    #+#             */
-/*   Updated: 2024/06/24 15:44:22 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/06/25 09:18:41 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "RNP.hpp"
+#include "RPN.hpp"
+
+/*****************************************************************************/
+/*                      CONSTRUCTORS & DESTRUCTOR                            */
+/*****************************************************************************/
+
+RPN::RPN(){};
+
+RPN::~RPN(){};
+
+RPN::RPN(RPN const &src)
+{
+    *this = src;
+    return ;
+}
+
+RPN::RPN(std::stack<double>numbers) :  numbers(numbers){};
+
+/*****************************************************************************/
+/*                           ASSIGNEMENT OPERATOR                            */
+/*****************************************************************************/
+
+RPN& RPN::operator=(const RPN& src)
+{
+    this->numbers = src.numbers;
+    return *this;
+}
+
+/*****************************************************************************/
+/*                            PUBLIC METHODS                                 */
+/*****************************************************************************/
+
+void RPN::compute(std::string calculus)
+{
+    for (size_t i = 0; i < calculus.size(); i++)
+    {
+        char element = calculus[i];
+        if (element == ' ')
+            continue;
+        else if (isdigit(element))
+        {
+            int nb = element - '0';
+            numbers.push(nb);
+        }
+        else if (element == '+' && numbers.size() >= 2)
+        {
+            double first = numbers.top();
+            numbers.pop();
+            double second = numbers.top();
+            numbers.pop();
+            numbers.push(first + second);
+        }
+        else if (element == '-' && numbers.size()>= 2)
+        {
+            double first = numbers.top();
+            numbers.pop();
+            double second = numbers.top();
+            numbers.pop();
+            numbers.push(second - first);
+        }
+        else if (element == '*' && numbers.size() >= 2)
+        {
+            double first = numbers.top();
+            numbers.pop();
+            double second = numbers.top();
+            numbers.pop();
+            numbers.push(first * second);
+        }
+        else if (element == '/' && numbers.size() >= 2)
+        {
+            double first = numbers.top();
+            numbers.pop();
+            double second = numbers.top();
+            numbers.pop();
+            numbers.push(second / first);
+        }
+        else
+        {
+            std::cout << RED BOLD "Invalid input" RESET << std::endl;
+            return;
+        }
+    }
+    if (numbers.size() == 1)
+    {
+        double result = numbers.top();
+        numbers.empty();
+        std::cout << GREEN BOLD "Result : " << result << std::endl;
+        return ;
+    }
+    else
+    {
+        std::cout << RED BOLD "Invalid input" RESET << std::endl;
+        return ;
+    }
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,8 +6,24 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 15:44:05 by lgernido          #+#    #+#             */
-/*   Updated: 2024/06/24 15:44:13 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/06/25 09:10:20 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "RNP.hpp"
+#include "RPN.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << RED BOLD "Invalid syntax : ./RPN calculus" RESET << std::endl;
+        return (1);
+    }
+    else
+    {
+        std::string input = argv[1];
+        RPN decoding;
+        decoding.compute(input);
+        return (0);
+    }
+}
